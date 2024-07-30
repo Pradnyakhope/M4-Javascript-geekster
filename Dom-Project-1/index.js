dd98669a22b7a80c8a177de01905ed35ca052002
Delta Student <deltastudent@apnacollege.in>
2024-07-30 10:59:06 +0530

    Project of dom is added

---
+++ Dom-Project-1/index.js
@@ -0,0 +1,13 @@
+ 
+ //Get a refernece to the div element using its Id
+ var divElement = document.getElementById("mydiv");
+        
+ //Change the style of the div element
+ divElement.style.color = "blue";
+ divElement.style.fontSize = "18px";
+ divElement.style.backgroundColor = "yellow";
+ divElement.style.margin = "20px";
+ divElement.style.padding = "10px";
+ divElement.style.fontweight = "bold";
+ divElement.style.height = "200px";
+ divElement.style.width = "300px";
